f235881c7763c7d552b77c57c54ad2dd64e63e07
Steve Winton <stevewinton@gmail.com>
2020-03-06 17:27:37 -0600

    Automatically expose the screenshot as an artifact

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const path = require('path');
 const captureWebsite = require('capture-website');
 const core = require('@actions/core');
+const artifact = require('@actions/artifact');
 
 (async () => {
-  const dest = path.join(os.tmpdir(), 'screenshot.png');
+  // Write to temporary directory
+  const destFolder = os.tmpdir();
+  const destFile = 'screenshot.png';
+  const dest = path.join(destFolder, destFile);
 
+  // Capture and write to dest
   await captureWebsite.file('https://github.com/swinton', dest, {launchOptions: {executablePath: '/usr/bin/google-chrome'}});
-  core.setOutput('path', dest);
+
+  // Create an artifact
+  const artifactClient = artifact.create();
+  const artifactName = core.getInput('artifact-name');
+  const uploadResult = await artifactClient.uploadArtifact(artifactName, [destFile], destFolder);
 })();
